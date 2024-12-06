3809774cf9a7a7bce2d2e72c7ee1b38452aa83c3
sonny <gyeomsony@gmail.com>
2024-12-06 21:57:22 +0900

    ✨[Lv.1] 테이블뷰 더미데이터 및 TableViewCell 파일 생성

---
+++ PokemonPhoneBook/PokemonPhoneBook/TableViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  TableViewCell.swift
+//  PokemonPhoneBook
+//
+//  Created by 손겸 on 12/6/24.
+//
+
+import UIKit
+
+
--- PokemonPhoneBook/PokemonPhoneBook/ViewController.swift
+++ PokemonPhoneBook/PokemonPhoneBook/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ContactListViewController: UIViewController {
+    
+    // 테이블 뷰
+    let tableView = UITableView()
+    
+    // 더미 데이터
+    let contacts = [
+        ("sonny", "010-5555-6666"),
+        ("changSu", "010-1111-2222"),
+        ("siHyeon", "010-4444-5555"),
+        ("gangMin", "010-8888-9999"),
+        ("teaMin", "010-7777-9999"),
+        ("jiHyo", "010-6666-0000"),
+        ("Mini", "010-3333-4444")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            //test
+    
     }
 
+    
 
 }
 
