a391c9f0d05268303b9e19d5e074d1df116d87d9
sonny <gyeomsony@gmail.com>
2024-12-06 22:22:42 +0900

    ✨[Lv.1] cell 안에 프로필 이미지와 이름 레이블 추가 - sonny

--- PokemonPhoneBook/PokemonPhoneBook/TableViewCell.swift
+++ PokemonPhoneBook/PokemonPhoneBook/TableViewCell.swift
@@ -6,5 +6,30 @@
 //
 
 import UIKit
+import SnapKit
+
+class ContactTableViewCell: UITableViewCell {
+    
+    // 프로필 이미지
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.layer.borderColor = UIColor.gray.cgColor
+        imageView.layer.borderWidth = 1
+        return imageView
+    }()
+    
+    // 이름 레이블
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+        
+    }()
+    
+}
 
 
