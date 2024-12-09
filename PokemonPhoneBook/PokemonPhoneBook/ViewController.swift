db9bb2b9e0688ff679d1915d119cf8522ef0eb44
Your jeongmu <105642388+jeongmuya@users.noreply.github.com>
2024-12-09 18:19:21 +0900

    [Lv 1] ✨ 친구목록, 추가 버튼 구현 - jeongmu

--- PokemonPhoneBook/PokemonPhoneBook/ViewController.swift
+++ PokemonPhoneBook/PokemonPhoneBook/ViewController.swift
@@ -2,18 +2,58 @@
 //  ViewController.swift
 //  PokemonPhoneBook
 //
-//  Created by YangJeongMu on 12/6/24.
+//  Created by YangJeongMu on 12/9/24.
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // MARK: - UI Componetns
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "친구 목록"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    private let addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("추가", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        
+        
     }
+    // MARK: -Setup UI
+    private func setupUI() {
+        view.backgroundColor = .white
 
-
+        // Add Components
+        view.addSubview(titleLabel)
+        view.addSubview(addButton)
+        
+        // Layout Using SnapKit
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.centerX.equalToSuperview()
+            
+        }
+        
+        addButton.snp.makeConstraints{ make in
+            make.centerY.equalTo(titleLabel)
+            make.trailing.equalToSuperview().inset(16)
+        }
+        
+    }
 }
 
